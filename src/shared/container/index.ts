8ee93af61b8aa7484f50d7c1176fecd520f9cbdd
Jorge Hecherat <hecherat@gmail.com>
2020-08-02 22:17:10 -0300

    [master] - Customers

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -10,7 +10,7 @@ import IOrdersRepository from '@modules/orders/repositories/IOrdersRepository';
 import OrdersRepository from '@modules/orders/infra/typeorm/repositories/OrdersRepository';
 
 container.registerSingleton<ICustomersRepository>(
-  'CustomerRepository',
+  'CustomersRepository',
   CustomersRepository,
 );
 
