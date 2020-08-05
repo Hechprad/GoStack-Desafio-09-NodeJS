71cf6bb009b21a9481f56d9e74ed721eb09ae9cb
Jorge Hecherat <hecherat@gmail.com>
2020-08-05 08:52:33 -0300

    [master] - Orders

--- src/modules/products/infra/typeorm/entities/Product.ts
+++ src/modules/products/infra/typeorm/entities/Product.ts
@@ -23,7 +23,7 @@ class Product {
   @Column('int')
   quantity: number;
 
-  // @OneToMany(() => OrdersProducts, order_products => order_products.product)
+  @OneToMany(() => OrdersProducts, order_products => order_products.product)
   order_products: OrdersProducts[];
 
   @CreateDateColumn()
